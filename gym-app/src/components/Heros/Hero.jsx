a2c26af82aacd53fe42dcb0d540c639fb9e26e97
gyanimishra <gyan12081997@gmail.com>
2022-09-24 13:04:22 +0530

    herosection-completed

--- gym-app/src/components/Heros/Hero.jsx
+++ gym-app/src/components/Heros/Hero.jsx
@@ -1,20 +1,88 @@
 import React from 'react'
 import Header from '../Header/Header'
 import './Hero.css'
+import hero_image from '../../assets/hero_image.png'
+import hero_image_back from '../../assets/hero_image_back.png'
+import Heart from '../../assets/heart.png'
+import Calories from '../../assets/calories.png'
 const Hero = () => {
   return (
     <div className='hero'>
 
         <div className="left-h">
             <Header/>
-        </div>
+            {/* The best Add */}
+            <div className="the-best-ad">
+              <div></div>
+              <span>the best fitness club in the town</span>
+            </div>
+            {/* Hero heading */}
+            <div className="hero-text">
+              <div>
+                <span className='stroke-text'>Shape  </span>
+                <span>Your</span>
+              </div>
 
+              <div>
+                <span>Ideal body</span>
+              </div>
+
+              <div className="span">
+                IN HERE WE WILL HELP YOU TO SHAPE AND BUILD YOUR IDEAL BODY AN LIVE UP YOUR LIFE TO FULLEST
+              </div>
+
+            </div>
+            {/* figure start//// */}
+
+            <div className="figures">
+              <div>
+                <span>+140</span>
+              <span>expert coaches</span>
+              </div>
+              <div>
+              <span>+978</span>
+              <span>members joined</span>
+              </div>
+              <div>
+              <span>+50</span>
+              <span>fitness Programes</span>
+              </div>
+            </div>
+          {/* hero buttons/////////////////// */}
+          <div className="hero-buttons">
+            <button className='btn'>Get Started</button>
+            <button className='btn'>Learn More</button>
+          </div>
+        </div>
+            {/* left h end */}
 
 
 
 
         <div className="right-h">
-            i am right
+           
+           <button className='btn'>Join Now</button>
+
+           <div className="heart-rate">
+            <img src={Heart} alt="" />
+            <span>Heart Rate</span><span>116 bpm</span>
+           </div>
+
+           {/* hero images */}
+
+           <img src={hero_image} alt="" className='hero-image' />
+           <img src={hero_image_back} alt="" className='hero-image-back' />
+
+           {/* calories div here........ */}
+
+           <div className="calories">
+            <img src={Calories} alt="" />
+            <div>
+            <span>Calories Burned</span>
+            <span>220 kcal</span>
+            </div>
+          
+           </div>
         </div>
       
     </div>
---
+++ gym-app/src/components/Programs/Programs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Programs.css'
+
+
+const Programs = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Programs
