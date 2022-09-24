2d114991fbf6a4f4eb2d648fedb533151120196e
gyanimishra <gyan12081997@gmail.com>
2022-09-25 02:05:19 +0530

    programs-section-completed

--- gym-app/src/components/Programs/Programs.jsx
+++ gym-app/src/components/Programs/Programs.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import './Programs.css'
-
+import {programsData} from '../../data/programsData'
+import RightArrow from '../../assets/rightArrow.png'
 
 const Programs = () => {
   return (
-    <div>
+    <div className='programs'>
+{/* programs headers//////// */}
+      <div className="programs-headers">
+        <span className='stroke-text'>Explore Our</span>
+        <span>Programe</span>
+        <span className='stroke-text'>to shape you</span>
+      </div>
+
+      {/* mapping data */}
+
+      <div className="program-categories">
+        {programsData.map((program)=>(
+          <div className="category">
+            {program.image}
+            <span> {program.heading}</span>
+              <span>{program.details}</span>
+              <div className="join-now"><span>Join Now</span> <img src={RightArrow} alt="" /></div>
+          </div>
+        ))}
+      </div>
+        
       
     </div>
   )
